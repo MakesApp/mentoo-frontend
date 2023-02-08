2418daa607f2ed5cf8c0d91e0adeec47517d8150
Meir J Cohen <meir84@gmail.com>
2023-02-08 13:10:34 +0200

    added conditional rendering, controlled inputs bind

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,7 +1,7 @@
+import { useState } from "react";
 import styled from "styled-components";
 import place from "./images/place.png";
 import volenteer from "./images/voloenteer.png";
-// import { useState } from "react";
 import useAuthContext from "./hooks/useAuthContext";
 // Styled Components
 const Header = styled.header`
@@ -30,19 +30,42 @@ const LogIn = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+
+const Input = styled.input`
+  margin: 0.5rem auto;
+  padding: 0.5rem;
+`;
+const A = styled.a`
+  text-decoration: none;
+  color: blue;
+  display: block;
+  &:hover {
+    color: CornflowerBlue;
+  }
+`;
 function HomePage() {
-  console.log("useAuthContext: ", useAuthContext());
-  const { role, setRole, dummyUser } = useAuthContext();
-  // setRole("place");
-  console.log("role: ", role);
-  console.log("dummyUser: ", dummyUser);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  //
+  const { role, setRole, dummyUser, isAuthenticated } = useAuthContext();
+  // console.log("useAuthContext: ", useAuthContext());
+  // console.log("role: ", role);
+  // console.log("dummyUser: ", dummyUser);
+  // Setting the role of volenteer or place
+  const handleRoleSelection = (role) => {
+    setRole(role);
+  };
 
-  // // State for the role selection - volenteer or place
-  // const [role, setRole] = useState("");
+  const handleNameChange = (e) => {
+    console.log("NAME e.target.value: ", e.target.value);
+    setUsername(e.target.value);
+  };
 
-  // const selectionHandler = (role) => {
-  //   setRole(role);
-  // };
+  const handlePasswordChange = (e) => {
+    console.log("PASSWORD e.target.value: ", e.target.value);
+    setPassword(e.target.value);
+  };
+
   return (
     <div>
       <Header>
@@ -51,39 +74,70 @@ function HomePage() {
         </h1>
         <h4>BY APPLESEEDS</h4>
       </Header>
-      <WelcomeContent>
-        <Selection>
+      {!isAuthenticated && !role && (
+        <WelcomeContent>
+          <Selection>
+            <div
+              onClick={() => {
+                handleRoleSelection("place");
+              }}
+            >
+              <img src={place} alt="place" />
+              <h3>מקום התנדבות</h3>
+            </div>
+          </Selection>
+          <Selection>
+            <div
+              onClick={() => {
+                handleRoleSelection("volenteer");
+              }}
+            >
+              <img src={volenteer} alt="volenteer" />
+              <h3>מתנדב</h3>
+            </div>
+          </Selection>
+        </WelcomeContent>
+      )}
+      {!isAuthenticated && role && (
+        <LogIn>
+          <Selection>
+            <div
+              onClick={() => {
+                handleRoleSelection("place");
+              }}
+            >
+              <img src={role === "volenteer" ? volenteer : place} alt={role} />
+              <h3>מקום התנדבות</h3>
+            </div>
+          </Selection>
+          <p>התחברות לחשבון המנטו שלך</p>
+          <form>
+            <Input
+              type="text"
+              placeholder="שם משתמש"
+              autoComplete="name"
+              value={username}
+              onChange={handleNameChange}
+            />
+            <br />
+            <Input
+              type="password"
+              placeholder="סיסמא"
+              autoComplete="current-password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </form>
           <div>
-            <img src={place} alt="place" />
-            <h3>מקום התנדבות</h3>
+            <button>שכחתי סיסמא</button>
+            <button>התחבר</button>
           </div>
-        </Selection>
-        <Selection>
-          <div>
-            <img src={volenteer} alt="volenteer" />
-            <h3>מתנדב</h3>
+          <div style={{ textAlign: "center" }}>
+            <p>אין לך חשבון?</p>
+            <A href="#">פתיחת חשבון מנטו</A>
           </div>
-        </Selection>
-      </WelcomeContent>
-      {/* THIS IS CURRENTLY DISPLAYING BOTH WELCOME AND LOGIN FOR DEV PURPUSES... THIS WILL BE HANDLED LATER*/}
-      <LogIn>
-        <p>התחברות לחשבון המנטו שלך</p>
-
-        <form>
-          <input type="text" placeholder="שם משתמש" autoComplete="name" />
-          <br />
-          <input
-            type="password"
-            placeholder="סיסמא"
-            autoComplete="current-password"
-          />
-        </form>
-
-        <div>
-          <button>התחבר</button>
-          <button>שכחתי סיסמא</button>
-        </div>
-      </LogIn>
+        </LogIn>
+      )}
     </div>
   );
 }
