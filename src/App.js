424ed3d1d28f89a036f80aff3d472b7658a4ce38
lidor12456 <44461724+lidor12456@users.noreply.github.com>
2023-02-08 15:23:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import IndexRoutes from "./components/IndexRoutes";
-import VolunteerPage from "./pages/volunteer page/components/VolunteerPage";
+
 import { Routes, Route, Link, useParams } from "react-router-dom";
 import ConfigRoutes from "./routes/ConfigRoutes";
 
 function App() {
   return (
     <div>
-      {/* <IndexRoutes /> */}
+     
       <ConfigRoutes />
       <nav className="">
         <li className="">
