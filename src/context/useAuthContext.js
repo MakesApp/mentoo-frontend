d6c2d5b54dc327ed4be8bc398db0d4b84ad882a4
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-05-18 13:36:09 +0300

    recreate porject

--- src/context/useAuthContext.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "./authContext";
-// custom hook to use the context
-function useAuthContext() {
-  return useContext(AuthContext);
-}
-
-export default useAuthContext;
--- src/pages/VolunteerPage/VolunteerPage.style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const DaysAndRegions = styled.div`
-  display: flex;
-  align-items: space-between;
-  justify-content: space-around;
-`;
