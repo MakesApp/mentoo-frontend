8577f20aa6393c8ffc0b5b282a92d494d7517d0e
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-05-31 08:23:18 +0300

    place ui is done

--- src/pages/Place/components/OldVolunteers/OldVolunteers.tsx
+++ src/pages/Place/components/OldVolunteers/OldVolunteers.tsx
@@ -1,4 +1,7 @@
+import Button from '../Button/Button'
 import List from '../List/List'
+import vIcon from '../../../../assets/images/v-icon.svg'
+
 interface User {
   id: string;
   profileImage: string;
@@ -11,7 +14,10 @@ interface ListProps {
 
 const OldVolunteers: React.FC<ListProps>  = ({users}) => {
   return (
-    <List users={users}>OldVolunteers</List>
+    <List users={users}>  <Button backgroundColor={"#792BA6"}>
+             <img src={vIcon} alt={"v icon"}/>
+             <span>למתנדבים שלי</span>
+         </Button></List>
   )
 }
 
