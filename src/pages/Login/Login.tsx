789165abe5eaf6b49363ffb2b04d796a3d6788b1
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-06-21 15:56:59 +0300

    change few things in spinner logic

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,14 +1,10 @@
 import  { useState } from 'react';
 import { useLoginMutation } from '../../api/services/api';
 import AuthForm from '../../components/AuthForm/AuthForm';
-import Spinner from '../../components/Spinner/Spinner';
-import { useAuthContext } from '../../context/useAuth';
 import style from './Login.module.css';
 
 const Login = () => {
-  const { mutateAsync,isLoading:loginLoading } = useLoginMutation();
-  const {isLoading:isUserLoading}=useAuthContext()
-  const isLoading=loginLoading||isUserLoading;
+  const { mutateAsync } = useLoginMutation();
   const [error, setError] = useState<string | undefined>();
   const handleLogin = async (email: string, password: string) => {
     try {
@@ -21,8 +17,7 @@ const Login = () => {
     }
   };
 
-  if(isLoading)
-  return <Spinner/>
+ 
 
   return (
     <div className={style.loginContainer}>
